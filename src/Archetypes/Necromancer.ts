68b965531e4134fe7eb87e976262c281dbf02342
yurigarrido <y.garrido.yuri@icloud.com>
2022-04-21 15:05:54 -0300

    requisito 05 - ok

---
+++ src/Archetypes/Necromancer.ts
@@ -0,0 +1,19 @@
+import { EnergyType } from '../Energy';
+import Archetypes from './Archetypes';
+
+class Necromancer extends Archetypes {
+  private static archetypesInstaces = 0;
+  private _energyType: EnergyType = 'mana';
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+  
+  static createdArchetypeInstances():number {
+    const countArchetypesInstaces = this.archetypesInstaces + 1;
+    this.archetypesInstaces = countArchetypesInstaces;
+    return this.archetypesInstaces;
+  }
+}
+
+export default Necromancer;
