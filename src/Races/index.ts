ba538e30964bccf9df510e2ac338f3e353b4c8de
yurigarrido <y.garrido.yuri@icloud.com>
2022-04-21 12:10:47 -0300

    requisito 03 - ok

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -5,5 +5,9 @@ import Elf from './Elf';
 import Halfling from './Halfling';
 import Orc from './Orc';
 
+import Energy, { EnergyType } from '../Energy';
+
+export { Energy, EnergyType }; 
+
 export { Dwarf, Elf, Halfling, Orc };
 export default Race;
